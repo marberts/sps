9d942069cdba4ce1ae67c6455b266e956d41b65b
Steve Martin <stevemartin041@gmail.com>
2022-10-26 23:45:42 -0400

    Added a test to compare sps_repweights() with bootstrapFP()

--- tests/test-repweights.R
+++ tests/test-repweights.R
@@ -22,3 +22,30 @@ try(sps_repweights(1:5, tau = NA))
 
 # Printing should preserve names
 sps_repweights(structure(rep(1, 5), names = letters[1:5]), 5)
+
+# Check against bootstrapFP:::generalised()
+# Fixed a bug with the exponential case by replaing exp() with rexp()
+bootstrapFP_fixed <- function(ys, pks, B) {
+  n <- length(ys)
+  ht <- vector("numeric", length = B)
+  w <- 1 / pks
+  for (b in seq_len(B)) {
+    a <- 1 + (rexp(n) - 1) * sqrt(1 - pks)
+    ws <- a * w
+    ht[b] <- sum(ys * ws)
+  }
+  HT <- sum(w * ys)
+  (sum((ht - HT))^2) / B
+}
+
+w <- 1 / c(1, runif(98), 1)
+y <- rlnorm(100)
+
+set.seed(51423)
+rw <- sps_repweights(w, 100, dist = function(x) rexp(x) - 1)
+var1 <- sum(colSums(rw * y) - sum(w * y))^2 / 100
+
+set.seed(51423)
+var2 <- bootstrapFP_fixed(y, 1 / w, 100)
+
+all.equal(var1, var2)
