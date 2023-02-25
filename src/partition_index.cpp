23c9d3cf67faa9647e02d88c134e66e7dae3fc15
Steve Martin <stevemartin041@gmail.com>
2023-02-24 20:16:16 -0500

    Removed Rcpp

--- src/partition_index.cpp
+++ src/partition_index.cpp
@@ -1,14 +1,18 @@
-#include <Rcpp.h>
-using namespace Rcpp;
+#include "cpp11.hpp"
+#include <numeric>
+#include <vector>
+using namespace cpp11;
 
-// [[Rcpp::export]]
-IntegerVector partition_index(NumericVector x, int n, bool decreasing) {
-  IntegerVector y = seq_along(x);
+[[cpp11::register]]
+
+std::vector<int> partition_index(doubles x, int n, bool decreasing) {
+  std::vector<int> res(x.size());
+  std::iota(res.begin(), res.end(), 1);
   std::nth_element(
-    y.begin(), y.begin() + n, y.end(),
+    res.begin(), res.begin() + n, res.end(),
     [x, decreasing](int i, int j) {
       return decreasing ? x[i - 1] > x[j - 1] : x[i - 1] < x[j - 1];
     }
   );
-  return y;
+  return res;
 }
