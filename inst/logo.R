fcfc7c78e7de0008aae5e20419e824e60c197ccc
Steve Martin <marberts@protonmail.com>
2025-08-07 00:07:51 -0400

    Added air.toml

--- inst/logo.R
+++ inst/logo.R
@@ -7,13 +7,15 @@ gg <- ggplot(data.frame(x = 1:5, y = 1:5, f = factor(c(1, 1, 1, 2, 2)))) +
   guides(fill = "none") +
   theme_void()
 
-sticker(gg,
-        package = "sps",
-        filename = "man/figures/logo.png",
-        s_x = 1,
-        s_width = 1,
-        p_size = 18,
-        p_family = "mono",
-        p_color = "#245B37",
-        h_fill = "#E2A328",
-        h_color = "#245B37")
+sticker(
+  gg,
+  package = "sps",
+  filename = "man/figures/logo.png",
+  s_x = 1,
+  s_width = 1,
+  p_size = 18,
+  p_family = "mono",
+  p_color = "#245B37",
+  h_fill = "#E2A328",
+  h_color = "#245B37"
+)
