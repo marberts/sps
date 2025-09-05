42117d202380c2e4fba2fb8aae5c3ab066f41a2d
Steve Martin <62676717+marberts@users.noreply.github.com>
2025-09-04 23:14:50 -0400

    Closes #10 (#11)

--- R/sps_iterator.R
+++ R/sps_iterator.R
@@ -4,8 +4,12 @@
 #' sequential Poisson method without replacing previously sampled units.
 #'
 #' @inheritParams sps
-#' @param ... Additional arguments to [`becomes_ta()`].
 #' @param n A positive integer giving the initial sample size for the iterator.
+#' @param alpha A number between 0 and 1. Units with
+#'   inclusion probabilities greater than or equal to 1 - `alpha` are set to 1.
+#'   The default is slightly larger than 0.
+#' @param cutoff A numeric cutoff. Units with `x >= cutoff` get
+#'   an inclusion probability of 1. The default does not apply a cutoff.
 #'
 #' @returns
 #' A function that returns the next unit in the sample. It take a single
@@ -22,7 +26,7 @@
 #' # Same as drawing the sample with 3 units.
 #' sps(1:5, 3, prn = prn)
 #' @export
-sps_iterator <- function(x, ..., n = 0L, prn = NULL) {
+sps_iterator <- function(x, n = 0L, prn = NULL, alpha = 0.001, cutoff = Inf) {
   x <- as.numeric(x)
   if (any(x <= 0)) {
     stop("sizes must be strictly greater than 0")
@@ -38,9 +42,12 @@ sps_iterator <- function(x, ..., n = 0L, prn = NULL) {
 
   s <- order(prn / x)
   pop <- seq_along(s)
-  bta <- split(pop, factor(becomes_ta(x, ...), levels = pop))
+  bta <- split(
+    pop,
+    factor(becomes_ta(x, alpha = alpha, cutoff = cutoff), levels = pop)
+  )
   if (n > 0L) {
-    sampled <- as.integer(sps(x, n, prn = prn))
+    sampled <- as.integer(sps(x, n, prn = prn, alpha = alpha, cutoff = cutoff))
     s <- s[!s %in% sampled]
   } else {
     sampled <- integer(0L)
