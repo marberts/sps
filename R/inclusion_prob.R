c07c0ed48e84e81becb46820272915696471fe95
Steve Martin <marberts@protonmail.com>
2026-08-15 22:22:25 -0400

    Updating eqn math

--- R/inclusion_prob.R
+++ R/inclusion_prob.R
@@ -3,9 +3,9 @@
 #' Calculate stratified (first-order) inclusion probabilities.
 #'
 #' Within a stratum, the inclusion probability for a unit is given by
-#' \eqn{\pi = nx / \sum x}{\pi = n * x / \sum x}. These values can be greater
+#' \eqn{\pi = nx / \sum x}. These values can be greater
 #' than 1 in practice, and so they are constructed iteratively by taking units
-#' with \eqn{\pi \geq 1 - \alpha}(from largest to smallest)
+#' with \eqn{\pi \geq 1 - \alpha} (from largest to smallest)
 #' and assigning these units an inclusion probability of 1, with the remaining
 #' inclusion probabilities recalculated at each step. See `vignette("take-all")`
 #' for details. If \eqn{\alpha > 0}, then
